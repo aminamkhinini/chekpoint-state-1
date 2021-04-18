eb8e7f7d7713006b8df7594ab39000295231e283
aminamkhinini <amina.mkhinini@gmail.com>
2021-04-19 00:50:53 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React, { Component } from 'react';
+import image from "./image.jpg"
+   
+   
+   
+ class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state ={
+      fullName:"Amina Mkhinini.",
+      bio:"I am a food engineering.", 
+      profession:"I am a future full-stack web developer.",
+      imgSrc: <img src={image} alt ='myImage'className="photo"/>,
+      show :false,
+      digit:0    
+    };
+  }
+show =()=> {
+  this.setState({ show:!this.state.show})
 }
+
+  componentDidMount=() =>{
+    setInterval(()=>{
+this.setState(prevState=>({digit: prevState.digit +1,}));
+    },1000);
+  };
+render() {
+    return (
+      <div className="App">
+       <h1> My Profil</h1> 
+       {this.state.show? 
+       <p className="profil">{this.state.fullName}
+       <br/> {this.state.bio}
+       <br/> {this.state.profession}
+       <br/> 
+       {this.state.imgSrc}
+       </p> : null}
+       <button onClick= {this.show} className="show-btn" > show me the profil </button>
+       <p className="count"> counter : {this.state.digit}</p>
+      </div>
+    )
+  }
+}
+
 
 export default App;
